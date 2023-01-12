68cb5cd1abfb2d66d12b9cf477728781e0e5e965
Shivani Mishra <shivaniuniverse@gmail.com>
2023-01-12 22:23:51 +0530

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -3,8 +3,7 @@ verb2 = input("verb - past participle: ")
 adj = input("adjective - something scary: ")
 year = input("year: ")
 obj = input("object - tree or an animal: ")
-madlib = f"I was {verb} there when I {verb2} her.
-I knew I should have warned her to not be so {adj}.
+madlib = f"I was {verb} there when I {verb2} her, I knew I should have warned her to not be so {adj}
 Because it has been going on since {year}. The year 
 our {obj} died..."
 
