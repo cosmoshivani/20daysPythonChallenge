3b5df3ad3238f10ddfa5a22e5e5372343f39fda8
Shivani Mishra <shivaniuniverse@gmail.com>
2023-01-12 22:23:01 +0530

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,8 +1,8 @@
-verb = input(verb - present participle: )
-verb2 = input(verb - past participle: )
-adj = input(adjective - something scary: )
-year = input(year: )
-obj = input(object - tree or an animal: )
+verb = input("verb - present participle: ")
+verb2 = input("verb - past participle: ")
+adj = input("adjective - something scary: ")
+year = input("year: ")
+obj = input("object - tree or an animal: ")
 madlib = f"I was {verb} there when I {verb2} her.
 I knew I should have warned her to not be so {adj}.
 Because it has been going on since {year}. The year 
